ae06029ebb2d448d04020a65bbeb8276cfc8eaf1
Edward Yang <jh.edwardyang@gmail.com>
2014-06-01 00:40:43 -0400

    yay

--- call.php
+++ call.php
@@ -1,28 +1,11 @@
 <?php
-    // Include the Twilio PHP library
-    require 'Services/Twilio.php';
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once('Services/Twilio.php'); // Loads the library
  
-    // Twilio REST API version
-    $version = "2010-04-01";
- 
-    // Set our Account SID and AuthToken
-    $sid = "ACeb64263658a378284735932fcc966cc1"; 
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "ACeb64263658a378284735932fcc966cc1"; 
 $token = "5b6fc237a813e94b05fc1d4856713596"; 
-     
-    // A phone number you have previously validated with Twilio
-    $phonenumber = '19027019264';
-     
-    // Instantiate a new Twilio Rest Client
-    $client = new Services_Twilio($sid, $token, $version);
+$client = new Services_Twilio($sid, $token);
  
-    try {
-        // Initiate a new outbound call
-        $call = $client->account->calls->create(
-            $phonenumber, // The number of the phone initiating the call
-            '5162340602', // The number of the phone receiving call
-            'http://ahri.walnutio.com/twiliostuff/ty.xml' // The URL Twilio will request when the call is answered
-        );
-        echo 'Started call: ' . $call->sid;
-    } catch (Exception $e) {
-        echo 'Error: ' . $e->getMessage();
-    }
+$call = $client->account->calls->create("+19027019264", "+15162340602", "http://ahri.walnutio.com/twiliostuff/ty.xml", array());
+echo $call->sid;
